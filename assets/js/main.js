c883c932559b550d3d5eb44e104bb3b74be9d77d
Daniella D <danielladevoto@gmail.com>
2018-09-09 21:03:33 -0400

    finished template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,16 @@
-$(document).ready(function () {
-    let page = window.location.href.split('/')[3];
-    console.log(page)
-    if (page == '#home') {
-      $('#home-link').addClass('active')
-      $('#home-link > a').css('color', 'black')
-    } else if (page == '#couple') {
-      $('#couple-link').addClass('active')
-      $('#couple-link > a').css('color', 'black')
-    } else if (page == '#wedding') {
-      $('#wedding-link').addClass('active')
-      $('#wedding-link > a').css('color', 'black')
-    } else if (page == '#rsvplink') {
-      $('#rsvp-link').addClass('active')
-      $('#rsvp-link > a').css('color', 'black')
-    }
-  })
+// $(document).ready(function () {
+//     let page = window.location.href.split('/')[3];
+//     console.log(page)
+//     if (page == '#home') {
+//       $('#home-link').addClass('active')
+//     } else if (page == '#couple') {
+//       $('#couple-link').addClass('active')
+//       $('#couple-link').css('background-color', 'rgb(252, 116, 138)')
+//     } else if (page == '#wedding') {
+//       $('#wedding-link').addClass('active')
+//       $('#wedding-link > a').css('color', 'black')
+//     } else if (page == '#rsvplink') {
+//       $('#rsvp-link').addClass('active')
+//       $('#rsvp-link > a').css('color', 'black')
+//     }
+//   })
